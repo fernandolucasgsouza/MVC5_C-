fa56ab755649469601834b6d90df4123c4caf362
Fernando Souza <fernandolucass.2012@gmail.com>
2018-05-07 22:54:54 -0300

    validacao formulario usuario v1

---
+++ Introducao/Introducao/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using Introducao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            var usuario = new Usuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            if (string.IsNullOrEmpty(usuario.Nome))
+            {
+                ModelState.AddModelError("Nome", "O Campo 'Nome' é de preenchimento obrigatório");
+            }
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", usuario);
+            }
+            return View(usuario);
+        }
+
+
+        public ActionResult Resultado(Usuario usuario)
+        {
+            return View(usuario);
+        }
+    }
+}
